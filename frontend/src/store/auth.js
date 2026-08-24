429a83d7c006429509121f0dbaf31323c7a0426c
margret256 <lwangamargret80@gmail.com>
2026-08-24 14:41:05 +0300

    refactor(frontend): update auth store

--- frontend/src/store/auth.js
+++ frontend/src/store/auth.js
@@ -1,6 +1,8 @@
 import { defineStore } from 'pinia'
 import axios from 'axios'
 
+const API_URL = 'http://localhost:5000/api'
+
 export const useAuthStore = defineStore('auth', {
   state: () => ({
     user: null,
@@ -8,21 +10,23 @@ export const useAuthStore = defineStore('auth', {
   }),
 
   actions: {
-    async login(email, password) {
-      const res = await axios.post('http://localhost:5000/api/auth/login', {
-        email,
+    async login(username, password) {
+      const res = await axios.post(`${API_URL}/auth/login`, {
+        username,
         password
       })
 
       this.user = res.data.user
       this.token = res.data.token
       localStorage.setItem('token', res.data.token)
+      localStorage.setItem('user', JSON.stringify(res.data.user))
     },
 
     logout() {
       this.user = null
       this.token = null
       localStorage.removeItem('token')
+      localStorage.removeItem('user')
     }
   }
 })
